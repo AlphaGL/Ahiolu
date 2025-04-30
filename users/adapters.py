3023d540593f451a2ebb135a9d50e03cae72d802
AlphaGL <156803189+AlphaGL@users.noreply.github.com>
2025-04-30 11:32:38 +0100

    Update adapters.py

--- users/adapters.py
+++ users/adapters.py
@@ -1,9 +1,11 @@
-from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.forms import ValidationError
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
         user.email = data.get('email')
-        user.username = None  # Prevent assigning a username
+        # Prevent setting username (Allauth tries to do this)
+        if hasattr(user, 'username'):
+            delattr(user, 'username')
         return user
