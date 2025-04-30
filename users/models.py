215d571d010724d292f63d3fd1075d8d1c94c49c
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-04-30 13:22:37 +0100

    phone_null_True

--- users/models.py
+++ users/models.py
@@ -23,7 +23,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)  # Email can be optional
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=15, unique=True)
+    phone = models.CharField(max_length=15, unique=True, null=True)
 
     objects = CustomUserManager()  # Attach custom manager
 
